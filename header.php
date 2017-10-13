6d8fafde83d42f0ab0bae984abe8568a5bebb352
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2017-10-13 16:21:03 +0200

    Started on a header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Roboto:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar">
+        <div class="icon"><a href="#"><i class="fa fa-home fa-4x"></i></a></div>
+        <div class="icon"><a href="#"><i class="fa fa-search fa-4x"></i></a></div>
+        <div class="icon"><a href="#"><i class="fa fa-upload fa-4x"></i></a></div>
+        <div class="icon"><a href="#"><i class="fa fa-user fa-4x"></i></a></div>
+        <div class="icon"><a href="#"><i class="fa fa-question-circle-o fa-4x"></i></a></div>
+      </nav>
+    </header>
+
+<script type="text/javascript">
+
+</script>
