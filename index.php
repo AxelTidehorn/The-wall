cd1c53fecae3442c4f92a6bf1ea25f46267e88de
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2017-10-15 20:31:20 +0200

    Started on the startpage

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php include 'header.php'; ?>
-
+<div class="pagecont">
+  <p>Welcome to this very awesome page where you can share all your creations.</p>
+  <h2><a href="#">Top Rated Content</a></h2>
+  <div class="contentcont">
+    <a href="#"><img src="imgs/An_Excellent_JPEG2.jpg" alt="an excellent picture"></a>
+    <div class="actioncont">
+      <div class="profilecont">
+        <a class="profilename" href="LINK-TO-PROFILE">Chef Excellence</a>
+      </div>
+      <div class="buttoncont">
+        <a class="likebtn" href="#">LIKE</a>
+      </div>
+    </div>
+  </div>
+  <div class="contentcont">
+    <a href="#"><img src="imgs/kebabpizza.jpg" alt="an excellent picture"></a>
+    <div class="actioncont">
+      <div class="profilecont">
+        <a class="profilename" href="LINK-TO-PROFILE">A. Whitedude</a>
+      </div>
+      <div class="buttoncont">
+        <a class="likebtn" href="#">LIKE</a>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include 'footer.php'; ?>
