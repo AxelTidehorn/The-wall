9f3839b2c15345f36d1d6eab87800a9faff468ea
linus97johansson <linus97johansson@gmail.com>
2017-10-16 14:57:04 +0200

    Push

---
+++ includes/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'psl-config.php';   // As functions.php is not included
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+/**
+ * Created by PhpStorm.
+ * User: Linus
+ * Date: 17-10-16
+ * Time: 14:31
+ */
--- includes/pls-config.php
+++ includes/pls-config.php
@@ -2,10 +2,10 @@
 /**
  * These are the database login details
  */
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "sec_user");    // The database username.
-define("PASSWORD", "eKcGZr59zAa2BEWU");    // The database password.
-define("DATABASE", "secure_login");    // The database name.
+define("HOST", "berzanappen.se");     // The host you want to connect to.
+define("USER", "berzanap_linus");    // The database username.
+define("PASSWORD", "ab92orre1");    // The database password.
+define("DATABASE", "berzanap_linus");    // The database name.
 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
