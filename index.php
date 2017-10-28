5f17ad8554f81d3f5710573ffadc642ff3d6c62d
Fractive <Fractive@live.com>
2017-10-28 18:20:48 +0200

    Database content presentation, content page and start of login changes
    The login appears to have some issues and that it doesn't look like you are logged in.
    
    Missing image at start is due to the blob images not being presented correctly yet.
    
    Newest content was also added as a new section.

--- index.php
+++ index.php
@@ -24,6 +24,66 @@
                     ';
                 }
             ?>
+
+                <div id="newest">
+                  <div class="center-text">
+                    <h2><a href="#">Newest Content</a></h2>
+                  </div>
+
+                    <?php
+                        include "includes/db_connect.php";
+                        $sql = $mysqli->prepare("SELECT Username, ContentImage FROM Content
+                                                JOIN Users ON Content.Publisher = Users.ID");
+                        $sql->bind_result($publisher, $contentImage);
+                        $sql->execute();
+
+                        while($sql->fetch()) { //How we do dis? Display blob images.
+                            echo '
+                                <div class="contentcont">
+                                    <a href="#"><img src="' . $contentImage . '" alt="an excellent picture"></a>
+                                    <div class="actioncont">
+                                      <div class="profilecont">
+                                        <a href="#" class="profilethumb"><img src="imgs/axel.jpg" alt="profilethumb"></a>
+                                        <a class="profilename" href="LINK-TO-PROFILE">' . $publisher . '</a>
+                                      </div>
+                                      <div class="buttoncont">
+                                        <a class="likebtn" href="#">LIKE</a>
+                                      </div>
+                                    </div>
+                                </div>
+                            ';
+                        }
+
+                        $sql->close();
+                    ?>
+
+                  <div class="contentcont">
+                    <a href="content.php"><img src="imgs/An_Excellent_JPEG2.jpg" alt="an excellent picture"></a>
+                    <div class="actioncont">
+                      <div class="profilecont">
+                        <a href="#" class="profilethumb"><img src="imgs/axel.jpg" alt="profilethumb"></a>
+                        <a class="profilename" href="LINK-TO-PROFILE">Chef Excellence</a>
+                      </div>
+                      <div class="buttoncont">
+                        <a class="likebtn" href="#">LIKE</a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="contentcont">
+                    <a href="#"><img src="imgs/kebabpizza.jpg" alt="an excellent picture"></a>
+                    <div class="actioncont">
+                      <div class="profilecont">
+                        <a href="#" class="profilethumb"><img src="imgs/axel.jpg" alt="profilethumb"></a>
+                        <a class="profilename" href="LINK-TO-PROFILE">A. Whitedude</a>
+                      </div>
+                      <div class="buttoncont">
+                        <a class="likebtn" href="#">LIKE</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
                 <div id="topRated">
                   <div class="center-text">
                     <h2><a href="#">Top Rated Content</a></h2>
