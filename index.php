bca52c15e86390aec0b5c00696a748b619e3bce5
Fractive <Fractive@live.com>
2017-11-10 21:12:22 +0100

    Front page content (not finished)

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
                 }
 
                 function loadContent($type) {
-                include_once "backend/connect.php";
+                    include "backend/connect.php";
 
-                if (!isset($_SESSION)) {
-                    session_start();
-                }
+                    if (!isset($_SESSION)) {
+                        session_start();
+                    }
 
                     //This will be used both to show ALL posts, and to show an induvidual posts!
                     //You will get the user id by a form input in the url. something like www.thewall.com/post?postID=2
@@ -78,62 +78,82 @@
                         $contentArray = array();
                         $count = 0;
 
-                        $where = "";
-                        if ($type == "Latest") {
-                            $where = " WHERE date = '" . date("Y-m-d") . "'";
+                        //$where = "";
+                        //if ($type == "Latest") {
+                            //$where = " WHERE date = '" . date("Y-m-d") . "'";
 
-                            for ($i = 1; $i < 7; $i++) {
-                                $where = $where . " OR date = '" . date("Y-m-d", strtotime("-" . $i . " Days")) . "'";
+                            if ($type == "Latest") {
+                                echo '<div id="newest">';
+                            } else if ($type == "Top Rated") {
+                                echo '<div id="topRated">';
+                            } else if ($type == "Editor's Choice") {
+                                echo '<div id="editorsChoice">';
                             }
-                        }
 
-                        $query = $conn->prepare("SELECT * FROM `Content`" . $where);
-                        $query->execute(); //Selecting both username and password may be redundant here as we are not really using that information apart from checking if there is some information.
-                        $query->store_result();
-                        $query->bind_result($id, $contentType, $publisher, $name, $url, $image, $webbsite, $text, $nsfw, $publicDomain, $rating, $date, $views, $description, $tags);
-
-
-                        //    trying to create a associative array with all the content. This is how im used to working.
-                        while ($query->fetch()) {
-                            $count++;
-                            $contentArray[$count] = array('ID' => $id, 'type' => $contentType, 'publisherID' => $publisher, 'name' => $name, 'url' => $url, 'image' => $image, 'webbsite' => $webbsite, 'text' => $text, 'nsfw' => $nsfw, 'publicDomain' => $publicDomain, 'rating' => $rating, 'date' => $date, 'views' => $views, 'description' => $description, 'tags' => $tags);
-                        }
-
-                        echo '<div id="newest">
-                            <div class="center-text">
-                                <h2><a href="#">' . $type . ' Content</a></h2>
-                            </div>
-                        ';
-
-                        foreach ($contentArray as $content) {
-                            $image = base64_encode(stripslashes($content['image']));
-                            $id = $content['ID'];
-                            print"
-                            <form action='post.php' method='get' class='form contentcont'>
-                                <input type='hidden' value='" . $id . "' name='post'/>
-                                <div onclick='this.parentNode.submit();'>
-                                    <img class='linkImg' src='data:image/jpeg;base64," . $image . "'/>
+                            echo '
+                                <div class="center-text">
+                                    <h2><a href="#">' . $type . ' Content</a></h2>
                                 </div>
-                                <div class='actioncont'>
-                                    <div class='profilecont'>
-                                        <a href='#' class='profilethumb'><img src='imgs/axel.jpg' alt='profilethumb'></a>
-                                        <a class='profilename' href='LINK-TO-PROFILE'>Chef Excellence</a>
-                                    </div>
-                                    <div class='buttoncont'>
-                                        <a class='likebtn' href='#'>LIKE</a>
-                                    </div>
-                                </div>
-                            </form>
-                        ";
+                            ';
+
+                            //for ($i = 0; $i < 7 * 24; $i++) {
+                                //$where = " WHERE date = '" . date("Y-m-d-H-i-s", strtotime("-" . $i . " Hours")) . "'";
+
+                                if ($type == "Editor's Choice") {
+                                    $query = $conn->prepare("SELECT * FROM `Content` WHERE editorsChoice = true");
+                                } else {
+                                    $query = $conn->prepare("SELECT * FROM `Content`");
+                                }
+                                $query->execute(); //Selecting both username and password may be redundant here as we are not really using that information apart from checking if there is some information.
+                                //$query->store_result();
+                                $query->bind_result($id, $contentType, $publisher, $name, $url, $image, $webbsite, $text, $nsfw, $publicDomain, $rating, $date, $views, $description, $tags, $editorsChoice);
 
 
-                        }
+                                //    trying to create a associative array with all the content. This is how im used to working.
+                                while ($query->fetch()) {
+                                    $count++;
+                                    $contentArray[$count] = array('ID' => $id, 'type' => $contentType, 'publisherID' => $publisher, 'name' => $name, 'url' => $url, 'image' => $image, 'webbsite' => $webbsite, 'text' => $text, 'nsfw' => $nsfw, 'publicDomain' => $publicDomain, 'rating' => $rating, 'date' => $date, 'views' => $views, 'description' => $description, 'tags' => $tags, 'editorsChoice' => $editorsChoice);
+                                }
+
+                                if ($type == "Latest") $contentArray = array_reverse($contentArray, true); //true to keep the keys of the array, seems to work without it though.
+                                //Simply reversing the array assuming they are added in chronological order to get the latest instead of making lots of sql queries to check different time frames.
+
+                                foreach ($contentArray as $content) {
+                                    $image = base64_encode(stripslashes($content['image']));
+                                    //$image = base64_encode(stripslashes($image));
+                                    $id = $content['ID'];
+                                    print"
+                                    <form action='post.php' method='get' class='form contentcont'>
+                                        <input type='hidden' value='" . $id . "' name='post'/>
+                                        <div onclick='this.parentNode.submit();'>
+                                            <img class='linkImg' src='data:image/jpeg;base64," . $image . "'/>
+                                        </div>
+                                        <div class='actioncont'>
+                                            <div class='profilecont'>
+                                                <a href='#' class='profilethumb'><img src='imgs/axel.jpg' alt='profilethumb'></a>
+                                                <a class='profilename' href='LINK-TO-PROFILE'>Chef Excellence</a>
+                                            </div>
+                                            <div class='buttoncont'>
+                                                <a class='likebtn' href='#'>LIKE</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                ";
+
+
+                                }
+
+                                echo '</div>';
+                            //}
+}
                         //Here is where we will display ALL the posts! We could use $_POST here to make you able to search among the users.
 
-                    };
+                    //};
                     }
 
                     loadContent("Latest");
+                    loadContent("Top Rated"); //This does not work currently, can be good if you can actually rate content first
+                    loadContent("Editor's Choice");
 
                 ?>
 
